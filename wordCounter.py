f022506c0ccd6b7bc6ea9e38268e42bfb7776a0d
Jordan Ott <jordanott365@gmail.com>
2017-09-15 15:16:32 -0700

    Comments

--- wordCounter.py
+++ wordCounter.py
@@ -17,23 +17,23 @@ import collections
 walk_dir = sys.argv[1]
 wordcount = collections.Counter()
 
-print('walk_dir = ' + walk_dir)
+#print('walk_dir = ' + walk_dir)
 
-print('walk_dir (absolute) = ' + os.path.abspath(walk_dir))
+#print('walk_dir (absolute) = ' + os.path.abspath(walk_dir))
 
 for root, subdirs, files in os.walk(walk_dir):
-    print('--\nroot = ' + root)
-    list_file_path = os.path.join(root, 'my-directory-list.txt')
-    print('list_file_path = ' + list_file_path)
+        #print('--\nroot = ' + root)
+        #list_file_path = os.path.join(root, 'my-directory-list.txt')
+        #print('list_file_path = ' + list_file_path)
 
-    with open(list_file_path, 'wb') as list_file:
-        for subdir in subdirs:
-            print('\t- subdirectory ' + subdir)
+        #with open(list_file_path, 'wb') as list_file:
+        #for subdir in subdirs:
+            #print('\t- subdirectory ' + subdir)
 
         for filename in files:
             file_path = os.path.join(root, filename)
 
-            print('\t- file %s (full path: %s)' % (filename, file_path))
+            #print('\t- file %s (full path: %s)' % (filename, file_path))
 
             with open(file_path) as f:
                 for line in f:
